4408ccb58e3587355e9893f3e2730ac51abf1bdc
zYuna646 <anggarangga646@gmail.com>
2024-09-10 11:54:41 +0800

    add: role, categori, holiday packages, and transaction model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role_id',
     ];
 
     /**
@@ -31,6 +32,12 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+    
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 
     /**
      * Get the attributes that should be cast.
